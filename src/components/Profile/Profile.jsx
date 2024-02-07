57954c55a7092278276fa4b7629305076b1c579d
RuslanVslv95 <ruslanaleks1995@gmail.com>
2024-02-07 22:05:07 +0300

    React#21(Верстаем страницу Диалогов)

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,17 +1,13 @@
 import s from './Profile.module.css';
 import MyPosts from './MyPosts/MyPosts';
+import ProfileInfo from "./MyPosts/Profileinfo/Profileinfo";
 
 const Profile = () => {
     return (
         <div>
-        <div>
-          <img alt='' src = 'https://st2.depositphotos.com/1005844/12089/i/450/depositphotos_120891408-stock-photo-sun-over-the-sea-scenery.jpg'/>
+            <ProfileInfo/>
+            <MyPosts/>
         </div>
-        <div>
-          ava + description
-        </div>
-        <MyPosts />
-      </div> 
     )
 }
 

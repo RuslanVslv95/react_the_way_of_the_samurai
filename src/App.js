cf3fbcb7f40f3debb604a2296889e261429a5f3d
RuslanVslv95 <ruslanaleks1995@gmail.com>
2024-04-17 22:29:24 +0300

    31. Уроки React JS - onClick, ref, VirtualDOM

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import {useLayoutEffect} from 'react';
+import { useLayoutEffect } from 'react';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
@@ -9,31 +9,29 @@ import Dialogs from "./components/Dialogs/Dialogs";
 import News from './components/News/News'
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import state from './redux/state';
 
 
 function App(props) {
     return (
-        <BrowserRouter>
 
-            <div className='app-wrapper'>
-                <Header />
-                <Navbar state={props.state.sidebar}/>
+        <div className='app-wrapper'>
+            <Header />
+            <Navbar state={props.state.sidebar} />
 
-                <div className='app-wrapper-content'>
-                    <Routes>
-                        <Route path='/dialogs' element={<Dialogs state={props.state.dialogsPage} />}/>
-                        <Route path='/profile' element={<Profile state={props.state.profilePage}/>}/>
-                        <Route path='/friends' element={<Friends state={props.state.sidebar}/>}/>
-                        <Route path='/news' element={<News/>}/>
-                        <Route path='/music' element={<Music/>}/>
-                        <Route path='/settings' element={<Settings/>}/>
-                    </Routes>
-                </div>
+            <div className='app-wrapper-content'>
+                <Routes>
+                    <Route path='/dialogs' element={<Dialogs state={props.state.dialogsPage} />} />
+                    <Route path='/profile' element={<Profile state={props.state.profilePage} />} />
+                    <Route path='/friends' element={<Friends state={props.state.sidebar} />} />
+                    <Route path='/news' element={<News />} />
+                    <Route path='/music' element={<Music />} />
+                    <Route path='/settings' element={<Settings />} />
+                </Routes>
             </div>
+        </div>
 
-        </BrowserRouter>
     );
 }
 
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,11 +2,19 @@ import s from './Dialogs.module.css'
 import DialogItem from './Dialogitem/Dialogsitem';
 import Message from './Message/Message';
 import { NavLink } from "react-router-dom";
+import React from 'react';
 
 const Dialogs = (props) => {
 
     let dialogsElements = props.state.dialogs.map(d => <DialogItem name={d.name} id={d.id} img={d.img} />);
     let messagesElements = props.state.messages.map(m => <Message message={m.message} id={m.id} />);
+
+    let textMessage = React.createRef();
+
+    let addMessage = () => {
+        let contentMessage = textMessage.current.value;
+        alert(contentMessage)
+    }
 
     return (
         <div className={s.dialogs}>
@@ -17,6 +25,13 @@ const Dialogs = (props) => {
 
             <div className={s.messages}>
                 {messagesElements}
+            </div>
+
+            <div>
+                <textarea ref={textMessage}></textarea>
+            </div>
+            <div>
+                <button onClick={addMessage}>Send Message</button>
             </div>
         </div>
     )

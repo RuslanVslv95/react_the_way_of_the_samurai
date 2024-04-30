49530ea8c15cb1972ecf03185c9b0121b0493c34
RuslanVslv95 <ruslanaleks1995@gmail.com>
2024-04-30 16:19:37 +0300

    34. Уроки React JS - Вынос мозга (FLUX-круговорот на каждый символ)

--- src/App.js
+++ src/App.js
@@ -23,7 +23,9 @@ function App(props) {
             <div className='app-wrapper-content'>
                 <Routes>
                     <Route path='/dialogs' element={<Dialogs state={props.state.dialogsPage} />} />
-                    <Route path='/profile' element={<Profile state={props.state.profilePage} addPost={props.addPost}/>} />
+                    <Route path='/profile' element={<Profile profilePage={props.state.profilePage}
+                                                             addPost={props.addPost}
+                                                             updateNewPostText={props.updateNewPostText}/>} />
                     <Route path='/friends' element={<Friends state={props.state.sidebar} />} />
                     <Route path='/news' element={<News />} />
                     <Route path='/music' element={<Music />} />
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -6,20 +6,25 @@ const MyPosts = (props) => {
 
     let newPostElement = React.createRef();
 
-    let addPost = () => {
-        let text = newPostElement.current.value;
-        props.addPost(text);
-        newPostElement.current.value = ' ';
-    }
 
     let postsElements = props.posts.map(p => <Post message={p.message} likesCount={p.likesCount} id={p.id} />)
+
+    let addPost = () => {
+        props.addPost();
+    }
+
+    let onPostChange = () => {
+        let text = newPostElement.current.value;
+        props.updateNewPostText(text);
+
+    }
 
     return (
         <div className={s.postsBlock}>
             <h3>My posts</h3>
             <div>
                 <div>
-                    <textarea ref={newPostElement}></textarea>
+                    <textarea onChange={onPostChange} ref={newPostElement} value={props.newPostText} />
                 </div>
                 <div>
                     <button onClick={addPost}>Add post</button>
